e34e03cec6168c71e11f1ea827c217fd49ffd32f
Mostafa Mosad <mostafamosad20172018@gmail.com>
2024-06-21 06:37:54 +0200

    ijo

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ Route::get('/', function () {
     return view('front.home');
 });
 
-Route::get('/mkh', function () {
-    return view('front.home');
-});
+
 
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
